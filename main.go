2f6fcf2580fafdc9422bb530ef56cfac71cfa3c8
aguedafrazao <aguedaaraujo13@gmail.com>
2020-06-11 21:07:18 -0300

    added imports

--- main.go
+++ main.go
@@ -1,79 +1,77 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 
-    "golang.org/x/oauth2"
-    "golang.org/x/oauth2/google"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
-var {
-    googleOauthConfig = &oauth.Config{
-        RedirectURL: "http://localhost:8080/callback",
-        ClientId: os.Getenv("GOOGLE_CLIENT_ID"),
-        ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-        Scopes: []string{"https://www.googleapis.com/auth/user.info.email"},
-        Endpoint: google.Endpoint,
-    }
-    // TODO: randomize it
-    randonState = "random"
+var (
+	googleOauthConfig *oauth2.Config
+	//TODO: randomize it
+	oauthStateString = "pseudo-random"
+)
+
+func init() {
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL: "http://localhost:8080/callback",
+		ClientID:    os.Getenv("GOOGLE_ID_CLIENT"),
+		ClientScret: is.Getenv("GOOGLE_CLIENT_SECRET"),
+		Scopes:      []string{"https://https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:    google.Endpoint,
+	}
 }
 
-
-func main(){
-    http.HandleFunc("/", handleHome)
-    http.HandleFunc("/login", handleLogin)
-    http.HandleFunc("/callback", handleCallback)    
-    http.ListenAndServe(":8080", nil)
+//server up
+func main() {
+	http.HandleFunc("/", handleMain)
+	http.HandleFunc("login", handleGoogleLogin)
+	hppt.HandleFunc("/callback", handleGoogleCallback)
+	fmt.Println(http.ListenAndServe(":8080", nil))
 }
 
-func handleHome(w.http.ResponseWriter, r *http.Request){
-    var html = `<html><body><a href="/login">Google Log In</a><body></html>`
-    fmt.Fprint(w, html)
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	var htmlIndex = `<html><body><a href="/login">Google Log In></body></html>`
+	fmt.Fprintf(w, htmlIndex)
 }
 
-func handleLogin(w.http.ResponseWriter, r *http.Request){
-    url := googleOauthConfig.AuthCodeUrl(randonState)
-    http.RedirectURL(w, r, url, http.StatusTemporaryRedirect)
+func handleGoogleLogin(w http.ResponseWriter, r *http.ReadRequest) {
+	url := googleOauthConfig.AuthCodeURL(oauthStateString)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 
-func handleCallback(w.http.ResponseWriter, r *http.Request){
-    if r.FormValue("state") != randonState {
-        fmt.Println("state is no valid")
-        http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-        return
-    }
-
-    token, err := googleOauthConfig.Exchange(oauth2.NoContext, r.FormValue("code"))
-    if err != nil {
-        fmt.Printf("could not get token: %s\n", err.Error())
-        http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-        return
-    }
-    
-    res, err := http.Get("http://www.googleapis.con/oauth2/v2/userinfo?access_token="+token.AccessToken)
-    if err != nil {
-        fmt.Printf("could not create get request: %s\n", err.Error())
-        http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-        return
-    }
-
-    defer resp.Body.Close()
-    content, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        fmt.Printf("could not parse response: %s\n", err.Error())
-        http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-        return
-    }
-
-    fmt.Fprint(w, "Response: %s", content)
-
-
-
+func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
+	content, err := getUserInfo(r.FormValue("state"), r.FormValue("code"))
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	fmt.Fprintf(w, "Content: %s\n", content)
 }
 
+func getUserInfo(state string, code string) ([]byte, error) {
+	if state != oauthStateString {
+		return nil, fmt.Errorf("invalid oauth state")
+	}
+	token, err := googleOauthConfig.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		return nil, fmt.Errorf("code exchange failed: %s", err.Error())
+	}
+	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+	}
 
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed reading response body: %s", err.Error())
+	}
 
+	return contents, nil
+}
